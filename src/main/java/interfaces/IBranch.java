5f28d842db697af53d12742ee1f4f37cc78267b9
amoroabd <amoroabd@gmail.com>
2021-03-18 16:01:55 +0000

    IBranch interface created

--- src/main/java/interfaces/IBranch.java
+++ src/main/java/interfaces/IBranch.java
@@ -1,4 +1,13 @@
 package interfaces;
+import classes.Customer;
+
+import java.util.ArrayList;
 
 public interface IBranch {
+    String getName();
+    ArrayList<Customer> getCustomers();
+    boolean newCustomer();
+    boolean addCustomerTransaction();
+    Customer findCustomer();
+
 }
