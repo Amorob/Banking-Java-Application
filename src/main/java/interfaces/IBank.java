328c349489ca8c4ca39b816b321529337f3c0df7
amoroabd <amoroabd@gmail.com>
2021-03-18 16:00:40 +0000

    IBank interface created

--- src/main/java/interfaces/IBank.java
+++ src/main/java/interfaces/IBank.java
@@ -1,4 +1,14 @@
 package interfaces;
+import classes.Branch;
 
 public interface IBank {
+    boolean addBranch(String branchName);
+
+    boolean addCustomer(String branchName, String customerName, double initialaAmount);
+
+    boolean addCustomerTransaction(String branchName, String customerName, double amount);
+
+    Branch findBranch(String branchName);
+
+    boolean listCustomers(String branchName, boolean showTransactions);
 }
